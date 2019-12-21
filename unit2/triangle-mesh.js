56eddc366122c0bd91759f6e81c8b6fed18fc9e1
yashkatta <yash.katta1@gmail.com>
2019-12-22 02:18:02 +0530

    Lesson 2 i. Triangle mesh

--- unit2/triangle-mesh.js
+++ unit2/triangle-mesh.js
@@ -14,11 +14,11 @@ var windowScale;
 function exampleTriangle() {
 	// This code demonstrates how to draw a triangle
 	var triangle = new THREE.Geometry();
-	triangle.vertices.push( new THREE.Vector3( 1, 1, 0 ) );
-	triangle.vertices.push( new THREE.Vector3( 3, 1, 0 ) );
-	triangle.vertices.push( new THREE.Vector3( 3, 3, 0 ) );
+	triangle.vertices.push(new THREE.Vector3(1, 1, 0));
+	triangle.vertices.push(new THREE.Vector3(3, 1, 0));
+	triangle.vertices.push(new THREE.Vector3(3, 3, 0));
 
-	triangle.faces.push( new THREE.Face3( 0, 1, 2 ) );
+	triangle.faces.push(new THREE.Face3(0, 1, 2));
 
 	return triangle;
 }
@@ -27,6 +27,13 @@ function drawSquare(x1, y1, x2, y2) {
 
 	var square = new THREE.Geometry();
 	// Your code goes here
+	square.vertices.push(new THREE.Vector3(x1, y1, 0));
+	square.vertices.push(new THREE.Vector3(x2, y1, 0));
+	square.vertices.push(new THREE.Vector3(x2, y2, 0));
+	square.vertices.push(new THREE.Vector3(x1, y2, 0));
+
+	square.faces.push(new THREE.Face3(0, 1, 2));
+	square.faces.push(new THREE.Face3(2, 0, 3));
 
 	// don't forget to return the geometry!	The following line is required!
 	return square;
@@ -48,59 +55,59 @@ function init() {
 	var windowWidth = windowScale * canvasRatio;
 	var windowHeight = windowScale;
 
-	camera = new THREE.OrthographicCamera(windowWidth/-2, windowWidth/2, windowHeight/2, windowHeight/-2, 0, 40);
+	camera = new THREE.OrthographicCamera(windowWidth / -2, windowWidth / 2, windowHeight / 2, windowHeight / -2, 0, 40);
 
-	var focus = new THREE.Vector3( 5,5,0 );
+	var focus = new THREE.Vector3(5, 5, 0);
 	camera.position.x = focus.x;
 	camera.position.y = focus.y;
 	camera.position.z = 20;
 	camera.lookAt(focus);
 
-	renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true});
+	renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
 	renderer.gammaInput = true;
 	renderer.gammaOutput = true;
-	renderer.setSize( canvasWidth, canvasHeight );
-	renderer.setClearColorHex( 0xFFFFFF, 1.0 );
+	renderer.setSize(canvasWidth, canvasHeight);
+	renderer.setClearColorHex(0xFFFFFF, 1.0);
 }
 
 function addToDOM() {
 	var container = document.getElementById('container');
 	var canvas = container.getElementsByTagName('canvas');
-	if (canvas.length>0) {
+	if (canvas.length > 0) {
 		container.removeChild(canvas[0]);
 	}
-	container.appendChild( renderer.domElement );
+	container.appendChild(renderer.domElement);
 }
 
 function render() {
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 }
 
 function showGrids() {
 	// Background grid and axes. Grid step size is 1, axes cross at 0, 0
-	Coordinates.drawGrid({size:100,scale:1,orientation:"z"});
-	Coordinates.drawAxes({axisLength:11,axisOrientation:"x",axisRadius:0.04});
-	Coordinates.drawAxes({axisLength:11,axisOrientation:"y",axisRadius:0.04});
+	Coordinates.drawGrid({ size: 100, scale: 1, orientation: "z" });
+	Coordinates.drawAxes({ axisLength: 11, axisOrientation: "x", axisRadius: 0.04 });
+	Coordinates.drawAxes({ axisLength: 11, axisOrientation: "y", axisRadius: 0.04 });
 }
 
 try {
 	init();
 	showGrids();
 	// creating and adding the triangle to the scene
-	var triangleMaterial = new THREE.MeshBasicMaterial( { color: 0x2685AA, side: THREE.DoubleSide } );
+	var triangleMaterial = new THREE.MeshBasicMaterial({ color: 0x2685AA, side: THREE.DoubleSide });
 	var triangleGeometry = exampleTriangle();
-	var triangleMesh = new THREE.Mesh( triangleGeometry, triangleMaterial );
+	var triangleMesh = new THREE.Mesh(triangleGeometry, triangleMaterial);
 	scene.add(triangleMesh);
 	// creating and adding your square to the scene !
-	var square_material = new THREE.MeshBasicMaterial( { color: 0xF6831E, side: THREE.DoubleSide } );
-	var square_geometry = drawSquare(3,5,7,9);
+	var square_material = new THREE.MeshBasicMaterial({ color: 0xF6831E, side: THREE.DoubleSide });
+	var square_geometry = drawSquare(3, 5, 7, 9);
 	var square_mesh = new THREE.Mesh(square_geometry, square_material);
 	scene.add(square_mesh);
 	addToDOM();
 	render();
-} catch(e) {
+} catch (e) {
 	var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
-	$('#container').append(errorReport+e);
+	$('#container').append(errorReport + e);
 }
 
 
